41884621ed635fd9d810e36a18fc93f4accda3db
Relapps <psadlowski28@gmail.com>
2026-04-26 20:22:59 +0200

    fix: comment out email form since neocities will not handle it anyway

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,8 @@
-import { ActionError, defineAction } from 'astro:actions';
-import { Resend } from 'resend';
-import {sendEmail} from './sendEmail'
+// import { ActionError, defineAction } from 'astro:actions';
+// import { Resend } from 'resend';
+// import {sendEmail} from './sendEmail'
 
 
-export const server = {
-  sendEmail
-};
+// export const server = {
+//   sendEmail
+// };
